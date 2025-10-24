ac75bfb95376ef27e55903c302ccb673d3418b82
devrocha03 <devrocha03@gmail.com>
2025-10-24 14:21:05 -0300

    aula4/Coment\303\241rios de c\303\263digo.js

---
+++ aula4/Comentários de código.js
@@ -0,0 +1,13 @@
+// Um comentário de uma única linha
+console.log("Olá, mundo!"); // Esta linha também é
+// comentário
+console.log("linha 1");
+// console.log("linha 2"); 
+
+/* 
+Este é um comentário de múltiplas linhas
+Um bloco de código comentado
+*/
+
+console.log("linha 3");
+
