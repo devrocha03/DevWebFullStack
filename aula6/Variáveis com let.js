10a43fb06353b9958ee16b1076695cd0c811d1df
devrocha03 <devrocha03@gmail.com>
2025-10-24 18:00:25 -0300

    Variáveis com Let

--- aula6/Variáveis com let.js
+++ aula6/Variáveis com let.js
@@ -1,1 +1,12 @@
-const
+let nome; // Declara a variável
+nome = "João"; // Atribui um valor à variável
+console.log(nome);
+nome = "Pedro"; // Reatribui o valor da variável
+console.log(nome);
+
+/* console.log(nome, "nasceu em 1990");
+console.log("Em 2000", nome, "conheceu Maria");
+console.log(nome, "casou-se com Maria em 2012");
+console.log("Maria teve 1 filho com", nome, "em 2015");
+console.log("O filho de", nome, "se chama Eduardo");
+ */
