8263cb4bd5fe3718e8bde620791bd815827fdd9b
devrocha03 <devrocha03@gmail.com>
2025-10-27 14:03:17 -0300

    Variáveis com Let e Regras para nomear variáveis com let

--- aula6/Variáveis com let.js
+++ aula6/Variáveis com let.js
@@ -1,12 +1,44 @@
-let nome; // Declara a variável
+/* let nome; // Declara a variável
 nome = "João"; // Atribui um valor à variável
 console.log(nome);
 nome = "Pedro"; // Reatribui o valor da variável
 console.log(nome);
 
-/* console.log(nome, "nasceu em 1990");
+console.log(nome, "nasceu em 1990");
 console.log("Em 2000", nome, "conheceu Maria");
 console.log(nome, "casou-se com Maria em 2012");
 console.log("Maria teve 1 filho com", nome, "em 2015");
 console.log("O filho de", nome, "se chama Eduardo");
  */
+
+
+    // Regras para nomear variáveis com let
+    
+// Não podemos criar variáveis com palavras reservadas
+    //let let;
+    //let if;
+
+// Variáveis precisam ter nomes significativos
+    //let nomeCompleto = "João Silva";
+    //let idadeDoUsuario = 25;
+    
+// Não podemos começar o nome de uma variável com um número
+    //let 1nome = "João";
+    
+// Não podem conter espaços ou traços
+    //let nome-completo = "João Silva";
+    
+// Utilizamos camelCase para nomear variáveis com mais de uma palavra
+    //let nomeCompletoDoUsuario = "João Silva";
+    
+// Variáveis são case-sensitive (diferenciam maiúsculas de minúsculas)
+    //let nomeCliente = "João";
+    //let nomecliente = "Pedro";
+    
+    //console.log(nomeCliente, nomecliente);
+    
+// Não podemos redeclarar variáveis com let
+
+// Não utilize VAR para declarar variáveis, UTILIZE LET!
+
+
