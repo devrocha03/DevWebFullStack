aea52b95d465a74533b93652770f746edc885212
devrocha03 <devrocha03@gmail.com>
2025-10-27 15:45:25 -0300

    Let vs Var - Primeira diferença

---
+++ aula9/Let vs Var - Primeira diferença.js
@@ -0,0 +1,5 @@
+nome = "João"; // NÃO FAÇA ISSO! Sempre declare suas variáveis com let ou const
+console.log(nome);
+
+// usando Let eu não posso redeclarar a variável
+// usando Var eu posso redeclarar a variável, mas isso não é uma boa prática
