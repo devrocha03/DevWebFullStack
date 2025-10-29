89eb6b2135aca0a41c941ddbebc3bfdc83e600a9
devrocha03 <devrocha03@gmail.com>
2025-10-29 16:28:47 -0300

    Alert, Confirm, Prompt + exec.

---
+++ aula12/alert, confirm e prompt.js
@@ -0,0 +1,25 @@
+// Usei parseFloat para aceitar números decimais
+
+/* let num1 = parseFloat(prompt("Digite um número: "));
+let num2 = parseFloat(prompt("Digite outro número: "));
+let resultado = num1 + num2;
+
+alert(`A soma é: ${resultado}`); */
+
+
+// Usei Number para converter os valores de string para número
+
+/* let num1 = prompt("Digite um número: ");
+let num2 = prompt("Digite outro número: ");
+let resultado = Number(num1) + Number(num2) + Number(num3);
+alert(`O Resultado é : ${resultado}`); */
+
+
+// Usei parseInt para aceitar apenas números inteiros
+
+/* let num1 = parseInt(prompt("Digite um número: "));
+let num2 = parseInt(prompt("Digite outro número: "));
+let resultado = num1 + num2;
+
+alert(`A soma é: ${resultado}`); */
+
