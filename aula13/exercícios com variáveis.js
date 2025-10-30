fa81ebbd28e07ec92601a224b0842864bc4141d9
devrocha03 <devrocha03@gmail.com>
2025-10-30 14:38:28 -0300

    exercícios com variáveis

---
+++ aula13/exercícios com variáveis.js
@@ -0,0 +1,10 @@
+// Substuir o valores da Variáveis
+
+let varA = "A"; // B
+let varB = "B"; // C
+let varC = "C"; // A
+
+[varA, varB, varC] = [varB, varC, varA]; // substitui o valor das variáveis
+
+console.log(varA, varB, varC);
+
