43965e57df76ba7f822ea02cdf161f89612a68a8
devrocha03 <devrocha03@gmail.com>
2025-11-04 13:29:22 -0300

    Objeto Math

---
+++ aula16/Objeto Math.js
@@ -0,0 +1,12 @@
+let num1 = 9.5365;
+
+// let num2 = Math.floor(num1); // função para arredondar para baixo
+// let num2 = Math.ceil(num1); // arredonda para cima
+// let num2 = Math.round(num1); // arrendonda pra cima ou pra baixo .50 arrendonda pra cima .49 arrendonda pra baixo o valor de forma automática
+// console.log(Math.max(1, 2, 3, 4, 10, 1400, -90, -10, 1300, 7, 8, 9)); // (Math.max) serve para pegar o maior número da sequencia
+// console.log(Math.min(1, 2, 3, 4, 10, 1400, -90, -10, 1300, 7, 8, 9)); // (Math.min) serve para pegar o menor número da sequencia
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // gera um número aleatória entre 10 e 5. Sendo que o 10 é o máx e o 5 é mínimo. 
+// (Math.random()) serve para gerar um número aleatória entre 0 e 1 e o 1 nunca é incluido
+// console.log(Math.PI); 
+
+
