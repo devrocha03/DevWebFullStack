73a9491a52bd18e7d4adb96cf9cdd22c0a52d581
devrocha03 <devrocha03@gmail.com>
2025-11-07 11:22:13 -0300

    Objetos

---
+++ aula20/Objetos.js
@@ -0,0 +1,53 @@
+// Para criar um objeto precisa abrir chaves {} e dentro da adiciona os atributos
+
+// !------------------------------------------------------------------------------------------
+/* const pessoa1 = {
+  nome: "João",
+  sobrenome: "Rocha",
+  idade: 35,
+
+  fala() {
+    console.log(`${this.nome} ${this.sobrenome} tem ${this.idade}`);
+  },
+
+  incrementarIdade() {
+    this.idade++;
+  },
+};
+
+pessoa1.fala();
+pessoa1.incrementarIdade();
+pessoa1.fala();
+pessoa1.incrementarIdade();
+pessoa1.fala();
+
+function criaPessoa(nome, sobrenome, idade) {
+  return { nome, sobrenome, idade };
+} */
+
+// !------------------------------------------------------------------------------------------
+
+/* const pessoa1 = criaPessoa("João", "Rocha", 35);
+const pessoa2 = criaPessoa("Maria", "James", 25);
+const pessoa3 = criaPessoa("John", "Tatum", 88);
+const pessoa4 = criaPessoa("Caio", "Wall", 55);
+
+console.log(pessoa1.nome, pessoa1.sobrenome, pessoa1.idade); */
+
+// !------------------------------------------------------------------------------------------
+
+/* const pessoa = { primeiroNome: "João", segundoNome: "Alexandre", idade: 35 }; */
+
+// !------------------------------------------------------------------------------------------
+
+/* const person = {
+    firstName: "João",
+    secondName: "Alexandre",
+    age: 35
+};
+const pessoas1 = {};
+pessoas1.primeiroNome = "João";
+pessoas1.segundoNome = "Alexandre"
+pessoas1.idade = 35; */
+
+// !------------------------------------------------------------------------------------------
